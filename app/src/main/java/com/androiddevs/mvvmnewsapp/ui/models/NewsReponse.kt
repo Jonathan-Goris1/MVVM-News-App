b827ae17ff161b991a37925b4e1090e30ff709f6
Jonathan Goris <jonathangoris123@gmail.com>
2022-03-23 17:43:59 -0400

    Added Click event Opens ArticleFragment.kt with clicked article

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/models/NewsReponse.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/models/NewsReponse.kt
@@ -3,7 +3,7 @@ package com.androiddevs.mvvmnewsapp.ui.models
 import com.androiddevs.mvvmnewsapp.ui.models.Article
 
 data class NewsReponse(
-    val articles: List<Article>,
+    val articles: MutableList<Article>,
     val status: String,
     val totalResults: Int
 )
